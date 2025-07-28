d31ded7dc343911627245e6d712ce918c794d894
Edwin <edwinnnzx@gmail.com>
2025-07-29 01:47:55 +0800

    initial commit

---
+++ src/assets/projects.ts
@@ -0,0 +1,59 @@
+export const projects = [
+  {
+    name: "Portfolio",
+    techs: [
+      "devicon-html5-plain colored",
+      "devicon-css3-plain colored",
+      "devicon-javascript-plain colored",
+    ],
+    description:
+      "A Neovim-themed portfolio website showcasing my skills and projects.",
+    image: "/projects/portfolio.webp",
+    links: [
+      {
+        url: "https://github.com/ezerinz/ezerinz.github.io",
+        icon: "devicon-github-plain",
+      },
+    ],
+  },
+
+  {
+    name: "Skedul",
+    techs: ["devicon-flutter-plain colored", "devicon-realm-plain colored"],
+    description:
+      "Designed and developed a schedule planner app for tracking assignments and classes, using Riverpod for state management and Realm for local data storage.",
+    image: "/projects/skedul.webp",
+    links: [
+      {
+        url: "https://github.com/ezerinz/skedul",
+        icon: "devicon-github-plain",
+      },
+    ],
+  },
+  {
+    name: "Mizora Privilege",
+    techs: ["devicon-flutter-plain colored"],
+    description:
+      "Developed the app's frontend by translating UI designs into responsive and seamless interfaces, and integrated RESTful APIs",
+    image: "/projects/mizora.webp",
+    links: [
+      {
+        url: "https://play.google.com/store/apps/details?id=com.mizora.jewelry.privilege",
+        icon: "fa fa-brands fa-google-play",
+      },
+    ],
+  },
+  {
+    name: "The Posing Portal",
+    techs: ["devicon-flutter-plain colored"],
+    description:
+      "Sliced UI designs for specific features, implemented state management using GetX, and integrated Firebase Database.",
+    image: "/projects/posing-portal.webp",
+    links: [
+      {
+        url: "https://play.google.com/store/apps/details?id=io.fand.posingportal",
+        icon: "fa fa-brands fa-google-play",
+      },
+    ],
+  },
+];
